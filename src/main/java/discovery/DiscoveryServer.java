2faf12b10bbdff1504b6adcd2a3ffe72edab5c57
Johan Mickos <jmickos@bu.edu>
2016-12-14 15:54:29 +0100

    Clean up server

--- src/main/java/discovery/DiscoveryServer.java
+++ src/main/java/discovery/DiscoveryServer.java
@@ -7,6 +7,8 @@ import java.net.MulticastSocket;
 import java.net.UnknownHostException;
 
 public class DiscoveryServer extends Thread {
+    private static final int DEFAULT_SERVER_PORT = 8081;
+    private static final int DEFAULT_BASE_PORT = 8082;
 
     InetAddress mcastAddr;
     int basePort;
@@ -29,51 +31,43 @@ public class DiscoveryServer extends Thread {
         this.start();
     }
 
-    public void run () {
+    public void run() {
         /*
           Listen for discovery requests, receive and reply them.
         */
         try {
 
-            while (true)
-            {
+            while (true) {
                 byte[] buffer = new byte[1024];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 receiveSocket.receive(packet);
                 String data = new String(buffer, 0, packet.getLength());
                 System.out.println("Server: we got a response: " + data);
 
-                if(data.equals("discovery"))
-                {
+                if (data.equals("discovery")) {
                     // reply
                     String reply = "discovery-reply";
                     DatagramPacket hi = new DatagramPacket(reply.getBytes(), reply.length(), mcastAddr, basePort);
                     replySocket.send(hi);
                 }
             }
-        }
-        catch (IOException ex) {
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         try {
-
             InetAddress mcastAddr = InetAddress.getByName("230.0.0.88");
-            int basePort = 8082;
-            int serverPort = 0;
+            int basePort = DEFAULT_BASE_PORT;
+            int serverPort = DEFAULT_SERVER_PORT;
 
-            if(args.length > 0) {
+            if (args.length > 0) {
                 serverPort = Integer.parseInt(args[0]);
             }
-            else {
-                throw new Exception("Invalid argument exception");
-            }
-
             new DiscoveryServer(mcastAddr, basePort, serverPort);
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
