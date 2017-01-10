f21afc3910acf955d6771e34fdf8700bb81027a8
Johan Mickos <jmickos@bu.edu>
2017-01-10 12:26:41 +0100

    Update javadoc for Node

--- src/main/java/common/Node.java
+++ src/main/java/common/Node.java
@@ -3,28 +3,54 @@ package common;
 import java.io.Serializable;
 import java.net.InetAddress;
 
+/**
+ * Represents a device in a network, identified by an address and port.
+ */
 public class Node implements Serializable {
 
+    /**
+     * Address of this Node.
+     */
     private InetAddress address;
+    /**
+     * Available port on this node.
+     */
     private int port;
 
+    /**
+     * Creates a new Node with an address and a port.
+     * @param address the node's address
+     * @param port the node's available port
+     */
     public Node(InetAddress address, int port) {
         this.address = address;
         this.port = port;
     }
 
+    /**
+     * @return this node's address
+     */
     public InetAddress getAddress() {
         return address;
     }
 
+    /**
+     * @param address the address for this node
+     */
     public void setAddress(InetAddress address) {
         this.address = address;
     }
 
+    /**
+     * @return this node's port
+     */
     public int getPort() {
         return port;
     }
 
+    /**
+     * @param port this node's available port
+     */
     public void setPort(int port) {
         this.port = port;
     }
